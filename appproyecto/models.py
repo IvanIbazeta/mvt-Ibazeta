708c87c7312958e0c22fd19da6aa20767f59a32b
IvanIbazeta <ibazeta.ivan26@gmail.com>
2022-12-12 14:03:43 -0300

    Trabajo Completo

--- appproyecto/models.py
+++ appproyecto/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Familia(models.Model):
     nombre_apellido = models.CharField(max_length = 50)
-    fecha_nacimiento = models.DateField(default= 14/5/2001)
+    año_nacimiento = models.IntegerField()
     dni = models.IntegerField()
--- appproyecto/views.py
+++ appproyecto/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from .models import Familia
 from django.http import HttpResponse
+
 # Create your views here.
+
 def familiar(request):
-    familiar1 = Familia(nombre = "Sandra", fecha_nacimiento = 31/12/1995, dni = 20567865 )
+    familiar1 = Familia(nombre_apellido = "Juan Gonzales", año_nacimiento = 1995, dni = 20567865 )
     familiar1.save()
-    familiar2 = Familia(nombre = "Ivan", fecha_nacimiento = 26/7/2000, dni = 45794302)
+    familiar2 = Familia(nombre_apellido = "Ivan Messi", año_nacimiento = 2000, dni = 45794302)
     familiar2.save()
-    familiar3 = Familia(nombre = "Enrique", fecha_nacimiento = 20/9/1993, dni = 20730184)
+    familiar3 = Familia(nombre_apellido = "Emiliano Martinez", año_nacimiento = 1992, dni = 20730184)
     familiar3.save()
     lista_familiares = [familiar1, familiar2, familiar3]
 
-    return render(request,"AppProyecto/template.html", {"cursos" : lista_familiares})
+    return render(request,"template.html", {"familiares" : lista_familiares})
     return HttpResponse()
